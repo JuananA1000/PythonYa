66cfaa007874e1e4d92425c9b675dff823e253a9
Juanan <juanantonioamillopez@gmail.com>
2024-10-26 13:40:17 +0200

    9.2. Parámetros

---
+++ 09. Funciones/02. Parámetros/ej_5.py
@@ -0,0 +1,37 @@
+'''
+  Confeccionar una función que reciba tres enteros y los muestre ordenados de menor a mayor. En otra función solicitar 
+  la carga de 3 enteros por teclado y proceder a llamar a la primera función definida. 
+'''
+
+
+def deMenorAMayor(num1, num2, num3):
+
+    print(f'\nNúmeros sin ordenar: {num1} - {num2} - {num3} ')
+
+    if num1 < num2 and num1 < num3:
+        if num2 < num3:
+            print(f'\nNúmeros ordenados: {num1} - {num2} - {num3}')
+        else:
+            print(f'\nNúmeros ordenados: {num1} - {num3} - {num2}')
+    else:
+        if num2 < num3:
+            if num1 < num3:
+                print(f'\nNúmeros ordenados: {num2} - {num1} - {num3}')
+            else:
+                print(f'\nNúmeros ordenados: {num2} - {num3} - {num1}')
+        else:
+            if num1 < num2:
+                print(f'\nNúmeros ordenados: {num3} - {num1} - {num2}')
+            else:
+                print(f'\nNúmeros ordenados: {num3} - {num2} - {num3}')
+
+
+def main():
+    numero1 = int(input('Número [1]: '))
+    numero2 = int(input('Número [2]: '))
+    numero3 = int(input('Número [3]: '))
+
+    deMenorAMayor(numero1, numero2, numero3)
+
+
+main()
