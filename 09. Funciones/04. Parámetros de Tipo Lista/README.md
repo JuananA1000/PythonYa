0159c68e94596bcfd1f07425e01b1027411f5a18
Juanan <juanantonioamillopez@gmail.com>
2024-11-05 13:29:41 +0100

    readmes

---
+++ 09. Funciones/04. Parámetros de Tipo Lista/README.md
@@ -0,0 +1,19 @@
+## Parámetros de Tipo Lista
+
+Trabajando con **programación estructurada**, dividimos nuestro problema por partes implementando **funciones**. La comunicación entre funciones se produce mediante el envío de datos a través de **parámetros** y el valor que retorna la función.
+
+Hasta ahora hemos resuelto problemas enviando datos simples como **enteros**, **floats** y **cadenas de caracteres**. 
+
+Ahora veremos que una función puede recibir también **estructuras de datos** como parámetro.
+
+```
+def ordenar_lista(lista):
+    return sorted(lista)
+
+# Ejemplo de uso
+mi_lista = [5, 3, 8, 1, 2]
+lista_ordenada = ordenar_lista(mi_lista)
+
+print('Lista de menor a mayor: ', lista_ordenada)
+```
+
--- 09. Funciones/04. Parámetros de Tipo Lista/ej_2.py
+++ 09. Funciones/04. Parámetros de Tipo Lista/ej_2.py
@@ -2,3 +2,32 @@
   Crear y cargar por teclado en el bloque principal del programa una lista de 5 enteros. Implementar una función que 
   imprima el mayor y el menor valor de la lista.
 '''
+
+def el_mayor(lista):
+    mayor = lista[0]
+
+    for i in range(1, 5):
+        if lista[i] > mayor:
+            mayor = lista[i]
+    return mayor
+
+def el_menor(lista):
+    menor = lista[0]
+
+    for i in range(1, 5):
+        if lista[i] < menor:
+            menor = lista[i]
+    return menor
+
+def main():
+    lista = [10, 7, 3, 7, 2]
+
+    print(f'\nDada esta lista: {lista}.')
+
+    mayor_lista = el_mayor(lista)
+    print(f'El mayor de todos sus elementos es: {mayor_lista}.')
+
+    menor_lista = el_menor(lista)
+    print(f'El menor de todos sus elementos es: {menor_lista}.')
+
+main()
